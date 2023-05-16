ef291832a8710423d9aaf5921db6a1cad3dfe6cb
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2023-05-16 17:58:14 -0300

    criando projeto

---
+++ src/main/java/com/rodrigo/GeekDev/GeekDevApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.GeekDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/GeekDev/GeekDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.GeekDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
